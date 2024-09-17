3aaa55882c1903b47c4781e5134fc80927170ed8
Jeffreyj02 <jeffreyjbsa@gmail.com>
2024-09-17 12:46:28 -0400

    Testing env

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from  'react';
+import Image from "next/image";
+const Navbar = () => {
+    return (
+        <div>
+            <span>CinemaSystem</span>
+            <button>
+                <img src="/x_pix.svg" alt="test" />
+            </button>
+        </div>
+    );
+};
+export default Navbar
