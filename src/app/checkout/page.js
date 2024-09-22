8fc44fa20ebd2235225f96636051abdf29cdcf3a
Jeffreyj02 <jeffreyjbsa@gmail.com>
2024-09-22 15:53:26 -0400

    Sync to main

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -1,15 +1,9 @@
-'use client'
-{/* use client, components are server*/}
-
-import React, { useState } from 'react';
-import CheckoutMovieInfo from '../components/CheckoutMovieInfo';
-import OrderDetails from '../components/OrderDetails';
-import { Button } from 'react-bootstrap';
-import OrderConfirmation from '../components/OrderConfirmation';
-
+import React from "react";
+import CustomNavbar from "../components/CustomNavbar";
+import OrderDetails from "../components/OrderDetails";
+import CheckoutMovieInfo from "../components/CheckoutMovieInfo";
+import Checkout from "../components/Checkout";
 export default function Home() {
-    const [isCheckout, setIsCheckout] = useState(false);
-
     const placeholderMovie = {
         img: "https://m.media-amazon.com/images/I/91JnoM0khKL._AC_UF894,1000_QL80_.jpg",
         id: 1,
@@ -18,28 +12,16 @@ export default function Home() {
         synopsis: "A movie about space",
         rating: "PG-13"
     };
-
     const placeholderOrder = {
         location: "AMC Theater",
         seats: "A1, A2, A3",
         tickets: {adult: 2, child: 1, senior: 0, adultPrice: 10, childPrice: 5, seniorPrice: 7}
     };
 
-    const handleCheckout = () => {
-        setIsCheckout(true);
-    };
-
     return (
         <div>
-            <CheckoutMovieInfo movie={placeholderMovie} />
-            {!isCheckout ? (
-                <>
-                    <OrderDetails order={placeholderOrder} />
-                    <Button variant="primary" onClick={handleCheckout}>Checkout</Button>
-                </>
-            ) : (
-                <OrderConfirmation order={placeholderOrder} />
-            )}
+            <CustomNavbar />
+            <Checkout order={placeholderOrder}/>
         </div>
     );
 }
