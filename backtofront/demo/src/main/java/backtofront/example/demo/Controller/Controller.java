f001ee94f1d1f05203de25400bb98958e1bf9de3
c-pill <81812824+c-pill@users.noreply.github.com>
2024-10-23 19:53:18 -0400

    Fixes

--- backtofront/demo/src/main/java/backtofront/example/demo/Controller/Controller.java
+++ backtofront/demo/src/main/java/backtofront/example/demo/Controller/Controller.java
@@ -277,6 +277,7 @@ private static class UserProfileResponse {
 
 
 // Update user profile
+/*
 @PostMapping("/update-profile")
 public ResponseEntity<?> updateProfile(@RequestBody UpdateProfileRequest updateProfileRequest) {
     try {
@@ -331,103 +332,4 @@ private static class UpdateProfileRequest {
     /* public int setid(int id) {
         this.id = id;
     } */
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public boolean isRegisterForPromotions() {
-        return registerForPromotions;
-    }
-
-    public void setRegisterForPromotions(boolean registerForPromotions) {
-        this.registerForPromotions = registerForPromotions;
-    }
-}
-
-private static class UpdatePasswordRequest {
-    private String email;
-    private String currentPassword;
-    private String newPassword;
-
-    // Getters and Setters
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCurrentPassword() {
-        return currentPassword;
-    }
-
-    public void setCurrentPassword(String currentPassword) {
-        this.currentPassword = currentPassword;
-    }
-
-    public String getNewPassword() {
-        return newPassword;
-    }
-
-    public void setNewPassword(String newPassword) {
-        this.newPassword = newPassword;
-    }
-}
-
-
-
 }
