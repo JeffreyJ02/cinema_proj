4eebc085b2a10aabc47ce8c64023196051f236a9
c-pill <81812824+c-pill@users.noreply.github.com>
2024-11-18 23:52:53 -0500

    fix show_seat

--- backtofront/demo/src/main/java/backtofront/example/demo/ShowSeat/ShowSeat.java
+++ backtofront/demo/src/main/java/backtofront/example/demo/ShowSeat/ShowSeat.java
@@ -4,6 +4,9 @@ import backtofront.example.demo.Showing.Showing;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
@@ -12,7 +15,12 @@ import lombok.Data;
 @Entity
 public class ShowSeat {
     
-    @Column(name = "seat_id", unique=true)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "show_seat_id", unique = true, nullable = false)
+    private int showSeatId;
+
+    @Column(name = "seat_id")
     private String seatId; 
     
     @ManyToOne(fetch = FetchType.LAZY)
