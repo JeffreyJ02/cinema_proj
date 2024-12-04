3d94bbe945740e852b8fc80ba27a119f56c446ef
Curt Leonard <curtisleonard03@gmail.com>
2024-12-04 16:06:10 -0500

    fixed

--- backtofront/demo/src/main/java/backtofront/example/demo/User/UserService.java
+++ backtofront/demo/src/main/java/backtofront/example/demo/User/UserService.java
@@ -3,12 +3,19 @@ package backtofront.example.demo.User;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     // Updated registerUser method to include optional credit card fields
     public void registerUser(String firstName, String lastName, String email,
